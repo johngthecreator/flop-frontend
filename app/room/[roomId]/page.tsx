031243a9c759e304ada706cee6647a2edd620c46
John Gorriceta <johngorriceta@Johns-MacBook-Air.local>
2023-10-11 22:43:24 -0600

    api url

--- app/room/[roomId]/page.tsx
+++ app/room/[roomId]/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 import { io } from "socket.io-client";
-import { useEffect } from "react";
 
 export default function RoomId({ params }: { params: { roomId: string } }){
     if (!process.env.NEXT_PUBLIC_API_URL)return;
     const socket = io(process.env.NEXT_PUBLIC_API_URL)
 
-    useEffect(()=>{
-        socket.emit("joinRoom", params.roomId)
-    },[])
-
+    socket.emit("joinRoom", params.roomId)
     socket.on('connect',()=>{console.log("user connected")})
     socket.on('disconnect',()=>{console.log("user disconnected")})
     socket.on('broadcastMessage', msg => {console.log(msg)})
